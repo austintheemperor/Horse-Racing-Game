d0e3fc79ab89e632580dad1899606780556b8a75
austintheemperor <dirtbikepro118@gmail.com>
2026-04-16 08:49:10 -0500

    stuff

--- javaScript/javaScript_v1.js
+++ javaScript/javaScript_v1.js
@@ -1,11 +1,11 @@
 let money = 500
 
-horses = ["red", "blue", "green", "magenta"]
+horses = ["red", "blue", "green", "purple"]
 
 function bet(betAmount, horse) {
 
 }
 
 function horseWinner(horses) {
-
+    const randomItem = horses[Math.floor(Math.random() * horses.length)]
 }
