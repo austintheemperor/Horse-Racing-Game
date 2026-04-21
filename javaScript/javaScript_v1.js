6379cdb8072673d535968f2baaa723b35472fb76
austintheemperor <dirtbikepro118@gmail.com>
2026-04-21 08:18:11 -0500

    moving code around

--- javaScript/javaScript_v1.js
+++ javaScript/javaScript_v1.js
@@ -4,7 +4,6 @@ horses = ["red", "blue", "green", "purple"]
 
 const randomHorse = horses[Math.floor(Math.random() * horses.length)]
 
-
 function bet(betAmount, horse) {
     if (horse == randomHorse) {
         money += betAmount
