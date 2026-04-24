5b98b46f306bd010856879d39c3eea63550f5385
Sam <samsambear0@gmail.com>
2026-04-24 08:23:42 -0500

    hit right thingy for winner

--- javaScript/javaScript_v1.js
+++ javaScript/javaScript_v1.js
@@ -3,6 +3,8 @@ let money = 1000
 horses = ["red", "blue", "green", "purple", "pink"]
 
 const randomHorse = horses[Math.floor(Math.random() * horses.length)]
+const rect = horses.getBoundingClientRect();
+const hitRight = rect.right >= window.innerWidth;
 
 function bet(betAmount, horse) {
     if (horse == randomHorse) {
