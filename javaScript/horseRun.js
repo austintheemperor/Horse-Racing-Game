51f1a491c2e1579b2e59823549b6fd6392cf5eae
Sam <samsambear0@gmail.com>
2026-04-29 08:44:35 -0500

    fix the leg animation

--- javaScript/horseRun.js
+++ javaScript/horseRun.js
@@ -3,40 +3,40 @@
 const horses = document.querySelectorAll('.horse');
 
 function startRace() {
-    const horseSpeeds = {
-        red: 3 + Math.random() * 2,
-        blue: 3 + Math.random() * 2,
-        green: 3 + Math.random() * 2,
-        pink: 3 + Math.random() * 2,
-        purple: 3 + Math.random() * 2
-    };
+  const horseSpeeds = {
+    red: 3 + Math.random() * 2,
+    blue: 3 + Math.random() * 2,
+    green: 3 + Math.random() * 2,
+    pink: 3 + Math.random() * 2,
+    purple: 3 + Math.random() * 2
+  };
 
-    horses.forEach(horse => {
-        let position = 0;
-        let isRunning = false;
+  horses.forEach(horse => {
+    let position = 0;
+    let isRunning = false;
 
-        const horseName = horse.getAttribute('alt');
-        let runSpeed = 700 / horseSpeeds[horseName];
-        let speed = horseSpeeds[horseName] || 4;
+    const horseName = horse.getAttribute('alt');
+    let runSpeed = 700 / horseSpeeds[horseName];
+    let speed = horseSpeeds[horseName] || 4;
 
-        setInterval(() => {
-            horse.src = isRunning
-                ? `images/${horseName}Horse.png`
-                : `images/${horseName}HorseRun.png`;
-            isRunning = !isRunning;
-        }, runSpeed);
+    setInterval(() => {
+      horse.src = isRunning
+        ? `images/${horseName}Horse.png`
+        : `images/${horseName}HorseRun.png`;
+      isRunning = !isRunning;
+    }, runSpeed);
 
-        function moveHorse() {
-            position += speed;
-            horse.style.left = position + "px";
+    function moveHorse() {
+      position += speed;
+      horse.style.left = position + "px";
 
-            if (position < window.innerWidth) {
-                requestAnimationFrame(moveHorse);
-            }
-        }
+     if (position < window.innerWidth) {
+        requestAnimationFrame(moveHorse);
+      }
+    }
 
-        moveHorse();
-    });
+    moveHorse();
+  });
 }
 
 
