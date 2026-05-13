e2bc11f07b19a200f65492804a12d32d6f2551ec
Sam <samsambear0@gmail.com>
2026-05-13 08:49:17 -0500

    finishing the tutorial thing

--- javaScript/horseRun.js
+++ javaScript/horseRun.js
@@ -2,16 +2,75 @@
 
 const horses = document.querySelectorAll('.horse');
 let horseIntervals = [];
-function startRace() {
- horseIntervals.forEach(interval => clearInterval(interval));
-  horseIntervals = [];
-  const horseSpeeds = {
+let speedChart;
+let horseSpeeds = {};
+let horsePercents = {};
+
+function generateOdds() {
+
+  horseSpeeds = {
     red: 3 + Math.random() * 2,
     blue: 3 + Math.random() * 2,
     green: 3 + Math.random() * 2,
     pink: 3 + Math.random() * 2,
     purple: 3 + Math.random() * 2
   };
+  const total = Object.values(horseSpeeds).reduce((a, b) => a + b, 0);
+horsePercents = {
+    red: (horseSpeeds.red / total) * 100,
+    blue: (horseSpeeds.blue / total) * 100,
+    green: (horseSpeeds.green / total) * 100,
+    pink: (horseSpeeds.pink / total) * 100,
+    purple: (horseSpeeds.purple / total) * 100
+  };
+    updateChart();
+    }
+    function updateChart() {
+
+  const ctx = document.getElementById("speedChart");
+  if (!ctx) return;
+
+  if (speedChart) {
+    speedChart.destroy();
+  }
+
+  speedChart = new Chart(ctx, {
+    type: "bar",
+
+    data: {
+      labels: Object.keys(horsePercents),
+
+      datasets: [{
+        label: "Win Chance (%)",
+        data: Object.values(horsePercents),
+
+        backgroundColor: ["red", "blue", "green", "pink", "purple"]
+      }]
+    },
+
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            callback: function(value) {
+              return value.toFixed(1) + "%";
+            }
+          }
+        }
+      }
+    }
+  });
+}
+
+function startRace() {
+ horseIntervals.forEach(interval => clearInterval(interval));
+  horseIntervals = [];
+
+
 
   horses.forEach(horse => {
     let position = 0;
@@ -42,5 +101,8 @@ function startRace() {
     moveHorse();
   });
 }
+window.addEventListener("load", () => {
+  generateOdds();
+});
 
 
